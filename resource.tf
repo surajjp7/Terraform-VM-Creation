f0b071e31fbed897067a5aa87b4b76b5d6c7d969
surajjp7 <suraajjp@gmail.com>
2026-01-19 08:12:01 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_virtual_machine" "lin-vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/var/lib/jenkins/.ssh/id_rsa.pub")
   }
 
   os_disk {
