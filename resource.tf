e173677b7a43bcaf145559b47f8d677250a76e6b
surajjp7 <suraajjp@gmail.com>
2026-01-19 13:18:41 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_virtual_machine" "lin-vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file(var.ssh_public_key)
+    public_key = var.ssh_public_key
   }
 
   os_disk {
